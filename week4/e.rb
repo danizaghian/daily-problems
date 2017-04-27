3e6c7bbc7303a497e8706ad065efea77e1097602
Samantha Morco <samanthamorco@gmail.com>
2017-04-26 21:10:45 -0400

    week 4 complete

---
+++ week4/e.rb
@@ -0,0 +1,21 @@
+# Return the missing letter from a given range of letters passed into the method as a string. If there is no missing letter, the method should return nil. bonus: returns a string of all missing letters as a string. ex: find_missing_letter("ace") would return "bd", write your own test.
+
+# this can definitely be refactored.
+def find_missing_letter(string)
+  letters = ('a'..'z').to_a
+  missing_letters = ""
+  split_string = string.chars
+  letter_index = letters.index(string[0])
+  split_string.each do |letter|
+    unless letter == split_string[0]
+      unless letters.index(letter) == letter_index + 1
+        missing_letters += letters[letters.index(letter) - 1]
+      end
+    end
+    letter_index = letters.index(letter)
+  end
+  missing_letters
+end
+
+p find_missing_letter("opqrsuv")  #=> "t"
+p find_missing_letter("xyz")      #=> nil
