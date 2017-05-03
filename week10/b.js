adf0413df351f171cdcf169d4461447deeb4b3bd
Samantha Morco <samanthamorco@gmail.com>
2017-05-03 18:57:44 -0400

    refactor solution for a problem

--- week10/b.js
+++ week10/b.js
@@ -2,20 +2,35 @@
 numbers = [1, 1, 2, 2, 3, 4, 4, 5, 5]
 numbersCount = {}
 
-// JS
-for (var i = 0; i < numbers.length; i++) {
-  if (numbersCount[numbers[i]]) {
-    numbersCount[numbers[i]]++
-  } else {
-    numbersCount[numbers[i]] = 1
-  }
-}
+var array = [1, 1, 2, 2, 3, 4, 4, 5, 5];
+var index = 0;
 
-for (var key in numbersCount) {
-  if (numbersCount.hasOwnProperty(key)) {
-    if (numbersCount[key] === 1) {
-      console.log(key);
+for (i = 0; i < array.length; i++) {
+  if (i % 2 === 0) {
+    if (array[i] === array[i + 1]) {
+      index = 0;
+    } else {
+      index = i;
+      console.log(array[index]);
       break;
     }
   }
 }
+
+// another way
+// for (var i = 0; i < numbers.length; i++) {
+//   if (numbersCount[numbers[i]]) {
+//     numbersCount[numbers[i]]++
+//   } else {
+//     numbersCount[numbers[i]] = 1
+//   }
+// }
+
+// for (var key in numbersCount) {
+//   if (numbersCount.hasOwnProperty(key)) {
+//     if (numbersCount[key] === 1) {
+//       console.log(key);
+//       break;
+//     }
+//   }
+// }
